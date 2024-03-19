4ac8db22ba17db1b5ef009163c46e3470cbe076b
reeve <reeveblake@gmail.com>
2024-03-19 16:08:55 -0400

    add dependencies from gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.seleniumhq.selenium:selenium-java:4.18.1")
+    implementation("org.seleniumhq.selenium:selenium-firefox-driver:4.18.1")
+
+    implementation("io.github.bonigarcia:webdrivermanager:5.7.0")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
