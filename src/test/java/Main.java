316cf91890366c121c7e6e7a28d3bdc9a74d95d7
Reeve <reeveblake@gmail.com>
2024-04-04 22:45:45 -0400

    update

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.Getter;
+import lombok.NonNull;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
 public class Main {
-    static final Login login = new Login(System.getenv("HULU_USERNAME"), System.getenv("HULU_PASSWORD"));
-
-
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    private static boolean driverSetup = false;
+    @Getter
+    private static final Login login = new Login(
+            System.getenv("HULU_USERNAME"),
+            System.getenv("HULU_PASSWORD"),
+            System.getenv("HULU_PROFILE")
+    );
+    
+    public static WebDriver getDriver() {
+        if (!driverSetup) {
+            WebDriverManager.firefoxdriver();
+            driver = new FirefoxDriver();
+            
+            driver.manage().timeouts().implicitlyWait(Duration.of(10, ChronoUnit.SECONDS));
+            
+            driverSetup = true;
+        }
+        return driver;
+    }
+    
+    @NonNull
+    public static WebDriverWait getWait() {
+        if (wait == null) {
+            wait = new WebDriverWait(getDriver(), Duration.of(8, ChronoUnit.SECONDS));
+        }
+        
+        return wait;
+    }
 }
--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -1,18 +1,47 @@
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class SignInTest {
-    @Test
-    void huluWelcomePage() {
-        WebDriver driver = new FirefoxDriver();
-
+    private final Login login = Main.getLogin();
+    private final WebDriver driver = Main.getDriver();
+    private final WebDriverWait wait = Main.getWait();
+    
+    @Test(priority = -1)
+    void huluWelcomePage() throws InterruptedException {
         driver.get("https://www.hulu.com/welcome");
         driver.manage().window().maximize();
         driver.findElement(By.xpath("/html/body/div[1]/div/div/div/header/nav/div[2]/a")).click();
-
-        driver.findElement(By.xpath("//*[@id=\\'login-modal\\']/div[2]/div[2]/div/div/div[3]/label/div[2]/input")).sendKeys("Username");
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div[4]")).sendKeys("Password");
+        
+        // Username (when visible)
+        By username = By.xpath("/html/body/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div[3]/label/div[2]/input");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(username));
+        driver.findElement(username)
+                .sendKeys(login.username());
+        
+        // Password
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div[4]/label/div[2]/input"))
+                .sendKeys(login.password());
+        
+        Thread.sleep(500);
+        // Login button
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/button")).click();
+        
+        Thread.sleep(1000);
+        // Select profile (when visible)
+        By profile = By.xpath("/html/body/div[2]/div[1]/div/div[2]/div/div[2]/div/div/div[" + login.profileNumber() + "]/a/span");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(profile));
+        WebElement profileElement = driver.findElement(profile);
+        
+        Actions actions = new Actions(driver);
+        actions.moveToElement(profileElement).click().perform();
     }
 }
