ca49dc9266756b345368acbeab0974845da53e85
Reeve/Xwy <18086052+reeve567@users.noreply.github.com>
2024-04-09 10:50:40 -0400

    update with groups

--- src/test/java/DetailsTest.java
+++ src/test/java/DetailsTest.java
@@ -10,7 +10,8 @@ public class DetailsTest {
     private final Login login = Main.getLogin();
     private final WebDriver driver = Main.getDriver();
     private final WebDriverWait wait = Main.getWait();
-    @Test(priority = 1)
+
+    @Test(priority = 1, groups = "details")
     void addToMyStuffButton() throws InterruptedException{
         driver.get("https://www.hulu.com/welcome");
         driver.manage().window().maximize();
@@ -46,7 +47,7 @@ public class DetailsTest {
         Thread.sleep(2000);
     }
 
-    @Test(priority = 2)
+    @Test(priority = 2, groups = "details")
     void likeDislikeButtons() throws InterruptedException{
         // Click on the three dots button
         driver.findElement(By.id("MenuButton-629")).click();
@@ -74,7 +75,7 @@ public class DetailsTest {
 
     }
 
-    @Test(priority = 3)
+    @Test(priority = 3, groups = "details")
     void watchPartyButton() throws InterruptedException{
         // Clicks on the start watch party button
         driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div/div/div[2]/div[1]/div/div/div/div[3]/div/div/div/div[4]/div/div[2]/div/button"))
@@ -86,7 +87,7 @@ public class DetailsTest {
         Thread.sleep(2000);
     }
 
-    @Test(priority = 4)
+    @Test(priority = 4, groups = "details")
     void seasonsDropdown() throws InterruptedException{
         // Clicks on the seasons dropdown
         driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div/div/div[4]/div/div[1]/div")).click();
@@ -99,7 +100,7 @@ public class DetailsTest {
         Thread.sleep(3000);
     }
 
-    @Test(priority = 5)
+    @Test(priority = 5, groups = "details")
     void detailsTabs() throws InterruptedException{
         // Click on the "You May Also Like" button
         driver.findElement(By.id("You May Also Like")).click();
